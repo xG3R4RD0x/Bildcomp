5c0dd63d1e46ca692d9301575ac8bec0c359ec2f
XG3R4RD0X <gerardo.vasquez.p@gmail.com>
2025-05-05 18:37:51 +0200

    added gitignore + setup test file

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Print "Hola Mundo" to the terminal
+print("Hola Mundo")
+
+# Test pandas by creating a simple DataFrame
+data = {"Column1": [1, 2, 3], "Column2": ["A", "B", "C"]}
+df = pd.DataFrame(data)
+print("Testing pandas with a sample DataFrame:")
+print(df)
